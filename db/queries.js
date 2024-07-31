4e97c4bd540840cd35f5c2237ed7b36fa8db39df
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-07-31 11:48:56 -0500

    update database name

--- db/queries.js
+++ db/queries.js
@@ -2,20 +2,21 @@ const pool = require("./pool");
 
 async function createUser(username, message, date) {
   await pool.query(
-    "INSERT INTO messages (username, message, date) VALUES ($1, $2, $3)",
+    "INSERT INTO messages_i8oy (username, message, date) VALUES ($1, $2, $3)",
     [username, message, date],
   );
 }
 
 async function getAllMessages() {
-  const { rows } = await pool.query("SELECT * FROM messages");
+  const { rows } = await pool.query("SELECT * FROM messages_i8oy");
   return rows;
 }
 
 async function getMessage(id) {
-  const { rows } = await pool.query("SELECT * FROM messages WHERE id = $1", [
-    id,
-  ]);
+  const { rows } = await pool.query(
+    "SELECT * FROM messages_i8oy WHERE id = $1",
+    [id],
+  );
 
   return rows[0];
 }

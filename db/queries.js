4b806297cdf104f7228c1eef5556905b40b26a0c
Diego <34121369+Diego-dot-web@users.noreply.github.com>
2024-07-31 12:10:27 -0500

    update to use connection string

--- db/queries.js
+++ db/queries.js
@@ -2,21 +2,20 @@ const pool = require("./pool");
 
 async function createUser(username, message, date) {
   await pool.query(
-    "INSERT INTO messages_i8oy (username, message, date) VALUES ($1, $2, $3)",
+    "INSERT INTO messages (username, message, date) VALUES ($1, $2, $3)",
     [username, message, date],
   );
 }
 
 async function getAllMessages() {
-  const { rows } = await pool.query("SELECT * FROM messages_i8oy");
+  const { rows } = await pool.query("SELECT * FROM messages");
   return rows;
 }
 
 async function getMessage(id) {
-  const { rows } = await pool.query(
-    "SELECT * FROM messages_i8oy WHERE id = $1",
-    [id],
-  );
+  const { rows } = await pool.query("SELECT * FROM messages WHERE id = $1", [
+    id,
+  ]);
 
   return rows[0];
 }
